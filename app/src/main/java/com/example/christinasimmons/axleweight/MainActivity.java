189a75aab83cfc273b33aea4e280347e58493967
Christina Simmons <christinasimmons@WTA-MBP363-377.local>
2018-06-14 13:43:07 -0400

    new logic

--- app/src/main/java/com/example/christinasimmons/axleweight/MainActivity.java
+++ app/src/main/java/com/example/christinasimmons/axleweight/MainActivity.java
@@ -41,19 +41,50 @@ btn.setOnClickListener(new View.OnClickListener() {
         int factor = 0;
         int weight = Integer.parseInt(usrInput.getText().toString());
 
-        if (weight >= 1000)
-        {
-            Total = (1000 * .04);
-            factor = 1;
-            if (weight > 2000)
-            {
-                Total += (1000 * .06);
-                factor = 2;
-            }
-            Total += ((weight - (1000 * factor)) * .10);
-            textView.setText(Total + "");
+//        if (weight >= 1000)
+//        {
+//            Total = (1000 * .04);
+//            factor = 1;
+//            if (weight > 2000)
+//            {
+//                Total += (1000 * .06);
+//                factor = 2;
+//            }
+//            Total += ((weight - (1000 * factor)) * .10);
+//            textView.setText(Total + "");
+//
+//        }
 
+
+        //first case = weight < 1000
+        //2nd case = weight > 1000
+        //3rd case = weight > 2000
+
+        double firstTotal = 0.0;
+        double secondTotal = 0.0;
+
+        //handles 1st case < 1000
+        if (weight < 1000) {
+            Total = (weight * 0.02);
         }
+        //handles 2nd case > 1000
+        else if (weight >= 1000){
+            firstTotal = (weight - 1000);
+            Total = (1000 * .02);
+        }
+        if (firstTotal > 1000){
+            secondTotal = (firstTotal - 1000);
+            Total += (1000 * .04);
+        }
+        else if(firstTotal < 1000){
+            Total += (firstTotal * .06);
+        }
+        if(secondTotal > 0) {
+            Total += (secondTotal * .06);
+        }
+
+        textView.setText("$" + Total);
+
     }
 
 });
@@ -69,25 +100,29 @@ btn.setOnClickListener(new View.OnClickListener() {
 
                 int weight = Integer.parseInt(usrInput.getText().toString());
 
-                if (weight < 2000){
-                    Total = (weight * 0.2);
+                if (weight < 2000) {
+                    Total = (weight * 0.02);
                 }
-                if (weight >=2000){
+                else if (weight >= 2000){
                     firstTotal = (weight - 2000);
                     Total = (2000 * .02);
                 }
-                if (weight >= 3000){
+                if (firstTotal >= 3000){
                     secondTotal = (firstTotal - 3000);
                     Total += (3000 * .04);
                 }
-                if (weight > 5000){
-                    Total += (secondTotal * .10);
-                    textView.setText(Total + "");
+                else if(firstTotal < 3000){
+                    Total += (firstTotal * .04);
                 }
-            }
+                if(secondTotal > 0) {
+                Total += (secondTotal * .10);
+                }
+                textView.setText("$" + Total);
+
+            }});
 
 
-            });
+
         Button clearBtn = findViewById(R.id.clear);
         clearBtn.setOnClickListener(new View.OnClickListener() {
 
